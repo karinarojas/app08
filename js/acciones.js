64727a5401c01724a78f45657860b67d613bdbbc
karinarojas <karina3f.dsaupoo@gmail.com>
2016-05-19 14:27:30 -0500

    1subida
    aplicacion8

---
+++ js/acciones.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+
+$(document).ready(function(e){
+	document.addEventListener("deviceready", onDeviceReady, false);
+	function onDeviceReady(){
+		$('#localizar').on('tap',function(){
+			navigator.geolocation.getCurrentPosition(function (position){
+				$('#localizacion').html('Latitude: ' + position.coords.latitude + '/n' +
+				'Longitude:' + position.coords.longitude + '/n' +
+				'Altitude:' + position.coords.altitude + '/n' +
+				'Accuracy:' + position.coords.accuracy + '/n' +
+				'Altitude Accuracy:' + position.coords.altitudeAccuracy + '/n' +
+				'Heading:' + position.coords.speed +'/n' +
+				'Speed:' + position.coords.speed + '/n' +
+				'Timestamp:' + position.timestamp + '/n');
+		}, $('#localizacion').html('Error en la localizacion'));
+		});
+		}
+		});
+		
